81660c9ec8611b6010094cce8a17bde80bd6f166
Tyler Kendrick <145080887+Tyler-R-Kendrick@users.noreply.github.com>
2024-07-30 19:52:33 +0000

    implemented fsspec

--- src/agents/agent.py
+++ src/agents/agent.py
@@ -1,24 +1,32 @@
+from dataclasses import dataclass
 from logging import Logger
 from abc import abstractmethod
 from models.agent_configuration import AgentConfiguration
 
+@dataclass
+class AgentResponse:
+    """Class to represent a response from an agent"""
+    conversation: list
+    response: str | None
+    streaming: bool = False
+
+
 class Agent():
     """Base class for agents"""
     def __init__(self, 
-                 logger: Logger, 
-                 agent_configuration: AgentConfiguration
-    ):
+        logger: Logger, 
+        agent_configuration: AgentConfiguration
+    ) -> None:
         self._logger: Logger = logger
         self._agent_configuration: AgentConfiguration = agent_configuration
-        self._conversation:list = []
-
-        if self._agent_configuration.initial_message is not None:
-            self._conversation = [{"role": "system", "content": self._agent_configuration.persona},
-                         {"role": "assistant", "content": self._agent_configuration.initial_message}]
-        else:
-            self._conversation = [{"role": "system", "content": self._agent_configuration.persona}]
+        self._conversation: list = [
+                { "role": "system", "content": self._agent_configuration.persona },
+                { "role": "assistant", "content": self._agent_configuration.initial_message }
+            ] if self._agent_configuration.initial_message is not None else [
+                { "role": "system", "content": self._agent_configuration.persona }
+            ]
 
     @abstractmethod
-    def run(self) -> None:
+    def run(self, user_input: str | None, conversation=None, stream=False) -> AgentResponse:
         """Abstract method to run the agent"""
         pass
--- src/app/app.py
+++ src/app/app.py
@@ -1,4 +1,5 @@
 from typing import LiteralString
+from agent import AgentResponse
 import streamlit as st
 import os
 import json
@@ -16,14 +17,15 @@ from agents.smart_agent.smart_agent import Smart_Agent
 from models.agent_configuration import AgentConfiguration, agent_configuration_from_dict
 from models.settings import Settings
 import fsspec
+from fsspec.implementations.local import LocalFileSystem
 
 # Initialize smart agent with CODER1 persona
-settings: Settings = Settings()
-
+settings: Settings = Settings(_env_file=".env") # type: ignore
 fs: fsspec.AbstractFileSystem = fsspec.filesystem(protocol="file")
 with fs.open(path=settings.smart_agent_prompt_location, mode="r", encoding="utf-8") as file:
     agent_config_data = yaml.safe_load(stream=file)
-    agent_config: AgentConfiguration = agent_configuration_from_dict(data=agent_config_data)
+    agent_config: AgentConfiguration = agent_configuration_from_dict(
+        data=agent_config_data)
 
 search_client = SearchClient(
     endpoint=settings.azure_search_endpoint,
@@ -38,21 +40,23 @@ client = AzureOpenAI(
 )
 
 search_vector_function = SearchVectorFunction(
-        logger=Logger(name="search_vector_function"),
-        search_client=search_client,
-        client=client,
-        model=settings.openai_embedding_deployment,
-        image_directory=settings.smart_agent_image_path
+    logger=Logger(name="search_vector_function"),
+    search_client=search_client,
+    client=client,
+    model=settings.openai_embedding_deployment,
+    image_directory=settings.smart_agent_image_path
 )
 
 agent = Smart_Agent(
     logger=Logger(name="smart_agent"),
     client=client,
     agent_configuration=agent_config,
-    search_vector_function = search_vector_function
+    search_vector_function=search_vector_function,
+    fs=LocalFileSystem(),
 )
 
-st.set_page_config(layout="wide",page_title="Smart Research Copilot Demo Application using LLM")
+st.set_page_config(
+    layout="wide", page_title="Smart Research Copilot Demo Application using LLM")
 style: LiteralString = f"""
 <style>
     .stTextInput {{
@@ -64,14 +68,15 @@ style: LiteralString = f"""
 st.markdown(body=style, unsafe_allow_html=True)
 
 
-MAX_HIST= 3
+MAX_HIST = 3
 # Sidebar contents
 with st.sidebar:
 
     st.title('Deep RAG AI Copilot')
     st.markdown('''
     ''')
-    st.checkbox("Show AI Assistant's internal thought process", key='show_internal_thoughts', value=False)
+    st.checkbox("Show AI Assistant's internal thought process",
+                key='show_internal_thoughts', value=False)
 
     add_vertical_space(5)
     if st.button('Clear Chat'):
@@ -93,7 +98,7 @@ with st.sidebar:
     st.write('')
     st.write('')
 
-    st.markdown('#### Created by James N., 2024')
+    st.markdown(body='#### Created by James N., 2024')
     if 'history' not in st.session_state:
         st.session_state['history'] = []
     if 'input' not in st.session_state:
@@ -105,99 +110,103 @@ with st.sidebar:
     if 'solution_provided' not in st.session_state:
         st.session_state['solution_provided'] = False
 
-        
-user_input= st.chat_input("You:")
-## Conditional display of AI generated responses as a function of user provided prompts
+
+user_input = st.chat_input(placeholder="You:")
+# Conditional display of AI generated responses as a function of user provided prompts
 history = st.session_state['history']
 display_data = st.session_state['display_data']
-question_count=st.session_state['question_count']
+question_count = st.session_state['question_count']
 
 if len(history) > 0:
-    #purging history
-    removal_indices =[]
-    idx=0
-    running_question_count=0
-    start_counting=False # flag to start including history items in the removal_indices list
+    # purging history
+    removal_indices = []
+    idx = 0
+    running_question_count = 0
+    # flag to start including history items in the removal_indices list
+    start_counting = False
     for message in history:
         idx += 1
         message = dict(message)
         print("role: ", message.get("role"), "name: ", message.get("name"))
         if message.get("role") == "user":
-            running_question_count +=1
-            start_counting=True
-        if start_counting and (question_count- running_question_count>= MAX_HIST):
+            running_question_count += 1
+            start_counting = True
+        if start_counting and (question_count - running_question_count >= MAX_HIST):
             removal_indices.append(idx-1)
-        elif question_count- running_question_count< MAX_HIST:
+        elif question_count - running_question_count < MAX_HIST:
             break
-            
+
     # remove items with indices in removal_indices
     for index in removal_indices:
         del history[index]
-    question_count=0
+    question_count = 0
     for message in history:
         message = dict(message)
 
         if message.get("role") == "user":
-            question_count +=1 
-        if message.get("role") != "system" and message.get("role") != "tool" and message.get("name") is None and len(message.get("content")) > 0:
-            with st.chat_message(message["role"]):
-                    st.markdown(message["content"])
+            question_count += 1
+        if message.get("role") != "system" \
+            and message.get("role") != "tool" \
+                and message.get("name") is None \
+        and len(message.get("content") or []) > 0:
+            with st.chat_message(name=message["role"]):
+                st.markdown(body=message["content"])
         elif message.get("role") == "tool":
             data_item = display_data.get(message.get("tool_call_id"), None)
-            if  data_item is not None:
-                if type(data_item) is PlotlyFigure:
-                    st.plotly_chart(data_item)
-                elif type(data_item) is MatplotFigure:
-                    st.pyplot(data_item)
-                elif type(data_item) is pd.DataFrame:
-                    st.dataframe(data_item)
-
-
-
-
+            if data_item is not None:
+                data_item_type = type(data_item)
+                if data_item_type is PlotlyFigure:
+                    st.plotly_chart(figure_or_data=data_item)
+                elif data_item_type is MatplotFigure:
+                    st.pyplot(fig=data_item)
+                elif data_item_type is pd.DataFrame:
+                    st.dataframe(data=data_item)
 else:
-    history, agent_response = agent.run(user_input=None)
-    with st.chat_message("assistant"):
-        st.markdown(agent_response)
-    user_history=[]
+    response: AgentResponse = agent.run(user_input=None)
+    history = response.conversation
+    agent_response: str | None = response.response
+    with st.chat_message(name="assistant"):
+        st.markdown(body=agent_response)
+    user_history = []
 if user_input:
     st.session_state['solution_provided'] = False
     st.session_state['feedback'] = False
-    data: dict = []
-    with st.chat_message("user"):
-        st.markdown(user_input)
+    data: dict = {}
+    with st.chat_message(name="user"):
+        st.markdown(body=user_input)
         try:
-            stream_out, code, history, agent_response,data = agent.run(user_input=user_input, conversation=history, stream=False)
+            response = agent.run(user_input=user_input,
+                                 conversation=history, stream=False)
+            history = response.conversation
+            agent_response = response.response
+            stream_out: bool = response.streaming
         except Exception as e:
-            agent_response= None
+            agent_response = None
             print("error in running agent, error is ", e)
             if 'history' in st.session_state:
                 st.session_state['history'] = []
             if 'display_data' in st.session_state:
                 st.session_state['display_data'] = {}
 
-    with st.chat_message("assistant"):
-        json_response=None
+    with st.chat_message(name="assistant"):
+        json_response = None
         if agent_response:
-            if "overall_explanation"  in agent_response:
+            if "overall_explanation" in agent_response:
                 try:
-                    agent_response= agent_response.strip("```json")
-                    json_response = json.loads(agent_response)
-                    st.markdown(json_response.get("overall_explanation")) 
+                    agent_response = agent_response.strip("```json")
+                    json_response = json.loads(s=agent_response)
+                    st.markdown(body=json_response.get("overall_explanation"))
                 except Exception as e:
                     print("exception json load ", e)
                     print(agent_response)
-                    st.markdown(agent_response)
+                    st.markdown(body=agent_response)
             if json_response:
                 for item in json_response:
-                    if item !="overall_explanation":
-                        image_path = os.path.join(".\\processed_data", item)
-                        st.markdown(json_response[item])
-                        st.image(image_path)
-
-
-    if data is not None:
-        st.session_state['display_data'] = data
+                    if item != "overall_explanation":
+                        image_path: str = os.path.join(
+                            ".\\processed_data", item)
+                        st.markdown(body=json_response[item])
+                        st.image(image=image_path)
 
 st.session_state['history'] = history
 st.session_state['question_count'] = question_count
