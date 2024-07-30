02ea358e6a73fdfeed72127a0c8073b70fd5e5d5
Pete Messina <petermessina@microsoft.com>
2024-07-30 15:03:11 -0400

    fixing api

--- src/api/api.py
+++ src/api/api.py
@@ -4,22 +4,17 @@ from fastapi import FastAPI
 from langserve import add_routes
 from langchain_core.runnables import RunnableLambda, RunnablePassthrough
 from langchain_community.vectorstores.azuresearch import AzureSearch
-from langchain_core.output_parsers import JsonOutputParser
 from models.settings import Settings
+from models.agent_response import AgentResponse
 from utils.smart_agent_factory import SmartAgentFactory
+from agents.smart_agent.smart_agent import Smart_Agent
 
-from typing import Any, Iterable
-from langchain_core.runnables import RunnableGenerator
-from agents.smart_agent.smart_agent import AgentResponse, Smart_Agent
-from langchain_core.messages import AIMessage, AIMessageChunk
+def deep_rag_search(question:str):
+    session_id = str(object=uuid.uuid4())
+    agent: Smart_Agent = SmartAgentFactory.create_smart_agent(settings=settings, session_id=session_id)
+    agent_response: AgentResponse = agent.run(user_input=question, conversation=[], stream=False)
 
-
-def streaming_parse(chunks: Iterable[str]) -> Iterable[str]:
-    for chunk in chunks:
-        yield chunk.content.swapcase()
-
-
-streaming_parse = RunnableGenerator(streaming_parse)
+    return agent_response.response
 
 app = FastAPI(
     title="LangChain Server",
@@ -29,26 +24,18 @@ app = FastAPI(
 
 settings: Settings = Settings()
 
+azureSearch = AzureSearch(
+    azure_search_endpoint="https://langchain.search.windows.net/",
+    azure_search_key="",
+    index_name="azureblob-index",
+    embedding_function="use",
+)
 
-def deep_rag_search(question:str):
-    session_id = str(object=uuid.uuid4())
-    agent: Smart_Agent = SmartAgentFactory.create_smart_agent(settings=settings, session_id=session_id)
-    agent_response: AgentResponse = agent.run(user_input=question, conversation=[], stream=False)
-
-    return agent_response.response
-
-#azureSearch = AzureSearch(
-#    azure_search_endpoint="https://langchain.search.windows.net/",
-#    azure_search_key="",
-#    index_name="azureblob-index",
-#    embedding_function="use",
-#)
-
-#add_routes(
-#    app=app,
-#    runnable=azureSearch.as_retriever(),
-#    path="/vectorRAG",
-#)
+add_routes(
+    app=app,
+    runnable=azureSearch.as_retriever(),
+    path="/vectorRAG",
+)
 
 add_routes(
     app=app,
